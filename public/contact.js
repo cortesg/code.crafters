84c5566bc971a051b239ad90ffc7c055e231a24e
Gino Cortes <gino.angelo.cortes@gmail.com>
2016-06-24 16:41:10 -0400

    contact page basic features ready with indenting

--- public/contact.js
+++ public/contact.js
@@ -4,16 +4,16 @@ function formclock() {
 
 
 	function setTColor() {
-			var formsub = document.getElementById("formsubmit");
-			var formmonocolors=["white", "black"]
-			// BLINKING SUBMIT
-			if(sec % 2 == 1) {
-				formsub.style.color=formmonocolors[1] 
-				formsub.style.backgroundColor=formmonocolors[0] 
-			} else if(sec % 2 == 0) {
-				formsub.style.color=formmonocolors[0]
-				formsub.style.backgroundColor=formmonocolors[1] 
-			}
+		var formsub = document.getElementById("formsubmit");
+		var formmonocolors=["white", "black"]
+		// BLINKING SUBMIT
+		if(sec % 2 == 1) {
+			formsub.style.color=formmonocolors[1] 
+			formsub.style.backgroundColor=formmonocolors[0] 
+		} else if(sec % 2 == 0) {
+			formsub.style.color=formmonocolors[0]
+			formsub.style.backgroundColor=formmonocolors[1] 
+		}
 	}
 	setTColor()
 }
@@ -23,19 +23,18 @@ formclock()
 
 //when clicking submit, it validates
 document.getElementById("formsubmit").onclick = function validate() {
-		//checks to see if email has an '@' symbol 
-		var Eform = document.getElementById('formemail').value
-		if (Eform.indexOf('@') == -1) {
-	      alert("Please provide a valid email.");
-	      return false; //forces to stay on page
-		}
-		//checks to see that text isn't blank
-		var Bform = document.getElementById('formbody').value
-		if (Bform==null || Bform=="") {
-	      alert("Please fill out text form.");
-	      return false;
-	    }
-	
+	//checks to see if email has an '@' symbol 
+	var Eform = document.getElementById('formemail').value
+	if (Eform.indexOf('@') == -1) {
+		alert("Please provide a valid email.");
+	    return false; //forces to stay on page if wrong
+	}
+	//checks to see that text isn't blank
+	var Bform = document.getElementById('formbody').value
+	if (Bform==null || Bform=="") {
+	    alert("Please fill out text form.");
+	    return false;
+	}
 }
 
 
